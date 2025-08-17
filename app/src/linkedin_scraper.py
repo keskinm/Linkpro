352d3189ae17aa1395f40ad1ef9178fd90409eda
Sasha Keskin <skeskin@bwi.earth>
2025-08-17 13:57:28 +0200

    Remove unused imports

--- app/src/linkedin_scraper.py
+++ app/src/linkedin_scraper.py
@@ -1,8 +1,7 @@
 import random
 import time
 
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
